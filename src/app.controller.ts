9592db24e73c703223599cfba96704884f7bc66c
Simon Orzel <simon.orzel@dynabase.de>
2022-05-08 17:29:37 +0200

    feat: adds base nutrition table type and output

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { AppService, NutritionTable } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getNutritionTable(): string {
+  getNutritionTable(): NutritionTable {
     return this.appService.getNutritionTable();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,37 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import * as convert from 'convert-units';
-
+export type NutritionTable = {
+  calories: number,               //	Energy	The number of calories.
+  carbohydrateContent: number,    //	Mass	The number of grams of carbohydrates.
+  cholesterolContent: number,     //	Mass	The number of milligrams of cholesterol.
+  fatContent: number,             //	Mass	The number of grams of fat.
+  fiberContent: number,           //	Mass	The number of grams of fiber.
+  proteinContent: number,         //	Mass	The number of grams of protein.
+  saturatedFatContent: number,    //	Mass	The number of grams of saturated fat.
+  servingSize: number,            //	Text	The serving size, in terms of the number of volume or mass.
+  sodiumContent: number,          //	Mass	The number of milligrams of sodium.
+  sugarContent: number,           //	Mass	The number of grams of sugar.
+  transFatContent: number,        //	Mass	The number of grams of trans fat.
+  unsaturatedFatContent: number,  //	Mass	The number of grams of unsaturated fat.
+};
 @Injectable()
 export class AppService {
-  getNutritionTable(): any {
-    return convert(1).from('l').to('ml');
+  getNutritionTable(): NutritionTable {
+    const nutritionTable: NutritionTable = {
+      calories: convert(1).from('l').to('ml'),
+      carbohydrateContent: 1,
+      cholesterolContent: 1,
+      fatContent: 1,
+      fiberContent: 1,
+      proteinContent: 1,
+      saturatedFatContent: 1,
+      servingSize: 1,
+      sodiumContent: 1,
+      sugarContent: 1,
+      transFatContent: 1,
+      unsaturatedFatContent: 1,
+    };
+    return nutritionTable;
   }
 }
