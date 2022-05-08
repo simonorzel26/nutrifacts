ef499b6c2ebe0b538e5695a8e65cb60f81fe0389
Simon Orzel <simon.orzel@dynabase.de>
2022-05-08 17:15:27 +0200

    feat: adds conversion lib

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import * as convert from 'convert-units';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): any {
+    return convert(1).from('l').to('ml');
   }
 }
