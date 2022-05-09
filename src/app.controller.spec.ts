c30b8cce7ce36d2dfd87e4f415b9df0e867121dd
Simon Orzel <simonorzel@gmail.com>
2022-05-09 21:26:37 +0200

    feat: refactors test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -2,28 +2,10 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import reqNutritionTableData from './shared/NutritionTable/reqNutritionTableData.json';
+import resNutritionTableData from './shared/NutritionTable/resNutritionTableData.json';
 import nutritionLabelTranslationEN from './i18n/en/nutrition-table.json';
 import path from 'path';
 import { AcceptLanguageResolver, I18nModule, QueryResolver } from 'nestjs-i18n';
-
-const exampleNutritionTableResponse = {
-  calories: { value: 100, unit: 'Kcal', label: 'Calories' },
-  carbohydrateContent: { value: 1, unit: 'g', label: 'Carbohydrates' },
-  cholesterolContent: { value: 1, unit: 'g', label: 'Cholesterol' },
-  fatContent: { value: 1, unit: 'g', label: 'Fat' },
-  fiberContent: { value: 1, unit: 'g', label: 'Fiber' },
-  proteinContent: { value: 1, unit: 'g', label: 'Protein' },
-  saturatedFatContent: { value: 1, unit: 'g', label: 'Saturated Fats' },
-  servingSize: { value: 1, unit: 'g', label: 'Serving Size' },
-  sodiumContent: { value: 1, unit: 'g', label: 'Sodium' },
-  sugarContent: { value: 1, unit: 'g', label: 'Sugars' },
-  transFatContent: { value: 1, unit: 'g', label: 'Trans Fats' },
-  unsaturatedFatContent: {
-    value: 1,
-    unit: 'g',
-    label: 'Unsaturated Fats',
-  },
-};
 
 describe('AppController', () => {
   let appController: AppController;
@@ -56,7 +38,7 @@ describe('AppController', () => {
         reqNutritionTableData,
       );
       expect(nutritionTable).toBeDefined();
-      expect(nutritionTable).toMatchObject(exampleNutritionTableResponse);
+      expect(nutritionTable).toMatchObject(resNutritionTableData);
     });
 
     it('should return properly translated labels and units"', async () => {
