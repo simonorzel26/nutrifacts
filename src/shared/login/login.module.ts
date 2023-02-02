881fe32a55fc37889048c674c7ff77903435f968
Tim Raderschad <tim.raderschad@gmail.com>
2023-02-02 15:23:07 +0100

    clean up code

--- src/shared/login/login.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LoginController } from './login.controller';
-
-@Module({
-  imports: [],
-  controllers: [LoginController],
-  providers: [],
-})
-export class LoginModule {}
